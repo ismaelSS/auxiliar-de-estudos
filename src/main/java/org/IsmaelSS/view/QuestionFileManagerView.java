be188cbc0575749c1839b342858853cb32b79aed
ismaelSS <ismael1337sn@gmail.com>
2026-07-13 13:39:28 -0300

    fix: transparent scroll-pane viewport, editor expands to fill space

--- src/main/java/org/IsmaelSS/view/QuestionFileManagerView.java
+++ src/main/java/org/IsmaelSS/view/QuestionFileManagerView.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 import java.util.ArrayList;
@@ -72,7 +73,7 @@ public class QuestionFileManagerView {
         scrollPane.setFitToWidth(true);
         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-        scrollPane.setPrefHeight(400);
+        VBox.setVgrow(scrollPane, Priority.ALWAYS);
 
         root.getChildren().addAll(fileSection, scrollPane);
     }
